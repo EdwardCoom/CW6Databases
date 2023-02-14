68604a794f4ce51c150fcad9c6113da0ee1e552c
Zach Coomer <zaccoom@ut.utm.edu>
2023-02-14 14:17:08 -0600

    Finally figured out how to incorporate info to display textbox correctly, needed to concatenate information.

--- CW6Databases/MainWindow.xaml.cs
+++ CW6Databases/MainWindow.xaml.cs
@@ -32,16 +32,18 @@ namespace CW6Databases
 
         private void Assets_Click(object sender, RoutedEventArgs e)
         {
-            string query = "select * from Assets";
+            string query = "select EmployeeID, AssetID, Description from Assets";
             OleDbCommand cmd = new OleDbCommand(query, cn);
             cn.Open();
             OleDbDataReader read = cmd.ExecuteReader();
+
             string data = "";
             while (read.Read())
             {
-                data += read[0].ToString() + "\n";
+                data += read[0].ToString() + " " + read[1].ToString() + " " + read[2].ToString() + "\n"; // concatenating all of the data to a string to be displayed
             }
-            DisplayBox.Text = data;
+            
+            AssetDisplayBox.Text = data; // where the data is being displayed.
             cn.Close();
         }
     }
